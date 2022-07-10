243ffef7c1361bd0eafa8fd29e20f585c89be3bf
James Pickering <james.pickering@gmail.com>
2022-07-10 19:29:31 +0100

    GUI shell and fetchAPI

--- README.md
+++ README.md
@@ -1,14 +1,16 @@
-# Server Time Client Specification
+# Time Client Specification
 
 The frontend application requires one page, split into two sections horizontally.
 On the left-hand side of the application, display:
-- The most recently-fetched value for server time (retrieved by hitting endpoint /time),
-displayed in epoch seconds.
-- The difference between current client machine time and the most recently-fetched value for server time in epoch seconds, formatted in stopwatch format (i.e. HH:mm:ss; a difference of 32 seconds would be 00:00:32, a difference of 0 seconds would be 00:00:00).
+
+- The most recently-fetched value for server time (retrieved by hitting endpoint /time), displayed in epoch seconds.
+
+- The difference between current client machine time and the most recently-fetched value for server time in epoch seconds, formatted in stopwatch format i.e. HH:mm:ss; a difference of 32 seconds would be 00:00:32, a difference of 0 seconds would be 00:00:00).
+
 - The displayed difference should update once per second. Eg. An initial difference of 00:00:00 would change after one second to 00:00:01.
 
 On the right-hand side of the application, display:
-- A HTML preformatted code block containing the most recently-fetched value of all Prometheus metrics (retrieved by hitting endpoint /metrics).
+A HTML preformatted code block containing the most recently-fetched value of all Prometheus metrics (retrieved by hitting endpoint /metrics).
 
 On loading the application for the first time, begin an API request to endpoints /time and /metrics to load current data values. 
 Every 30 seconds, the frontend application should make API requests to endpoints /time and /metrics to load the latest data.
--- src/App.js
+++ src/App.js
@@ -3,34 +3,63 @@ import moment from 'moment';
 import './App.css';
 
 export default function App() {
-  const [serverTime, setServerTime] = useState('HH:mm:ss');
+  const [loading, setloading] = useState(true);
+  const [serverTime, setServerTime] = useState('00:00:00');
+  const [clientTime, setClientTime] = useState('00:00:00');
+  // const [timeDifference, setTimeDifference] = useState('');
 
   const formatTime = (date) => {
     return moment(date).format('HH:mm:ss');
   };
 
-  useEffect(() => {
-    const data = fetchServerTime();
-    console.log(formatTime(data));
-    setServerTime(formatTime(data));
-  }, []);
-
-  const fetchServerTime = async () => {
-    fetch('http://localhost:4000/time/')
+  const fetchAPI = () => {
+    fetch('http://localhost:4000/time')
       .then((response) => {
         return response.json;
       })
       .then((data) => {
+        setloading(false);
         return data;
       })
       .catch((err) => {
+        setloading(false);
         console.log(err);
       });
   };
 
+  // TODO: 
+  useEffect(() => {
+    const data = fetchAPI();
+    setServerTime(formatTime(data));
+    setClientTime(formatTime(Date.now()));
+
+    setInterval(() => {
+      setClientTime(formatTime(Date.now()));
+    }, 1000);
+  }, []);
+
   return (
     <div className='App'>
-      <header className='App-header'>Server Time (HH:mm:ss): {serverTime}</header>
+      <header className='App-header'>
+        <h1>Time Machine</h1>
+      </header>
+      <main>
+        <p>{loading ? <span>loading...</span> : <></>}</p>
+        <div className='split left'>
+          <p>
+            Server Time: <code>{serverTime}</code> (last fetched)
+          </p>
+          <p>
+            Local Time: <code>{clientTime}</code>
+          </p>
+          {/* <p>
+            Diff Time: <code>{timeDiff}</code>
+          </p> */}
+        </div>
+        <div className='split metrics'>
+          <h3>Metrics</h3>
+        </div>
+      </main>
     </div>
   );
 }
